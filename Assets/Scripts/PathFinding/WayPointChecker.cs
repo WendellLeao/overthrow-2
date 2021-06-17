f84ed65bd541afc419481e254ee021121c28ff27
Wendell Leão <leaowendell@hotmail.com.br>
2021-06-17 14:13:25 -0300

    WayPoint system divided into two files, each with its own responsibility

---
+++ Assets/Scripts/PathFinding/WayPointChecker.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public sealed class WayPointChecker
+{
+    private WayPointSystem _wayPointSystem;
+    private Transform[] _wayPoints;
+
+    public WayPointChecker(WayPointSystem wayPointSystem, Transform[] wayPoints)
+    {
+        _wayPointSystem = wayPointSystem;
+        _wayPoints = wayPoints;
+    }
+
+    public bool IsAtTheTargetPoint()
+    {
+        return _wayPointSystem.transform.position == _wayPoints[_wayPointSystem.WayPointIndex].transform.position;
+    }
+
+    public bool IsAtTheLastPoint()
+    {
+        return _wayPointSystem.transform.position == _wayPoints[_wayPoints.Length - 1].position;
+    }
+}
--- Assets/Scripts/PathFinding/WayPointSystem.cs
+++ Assets/Scripts/PathFinding/WayPointSystem.cs
@@ -48,26 +48,4 @@ public sealed class WayPointSystem : MonoBehaviour
                 _wayPointIndex++;
         }
     }
-
-    private sealed class WayPointChecker : MonoBehaviour
-    {
-        private WayPointSystem _wayPointSystem;
-        private Transform[] _wayPoints;
-
-        public WayPointChecker(WayPointSystem wayPointSystem, Transform[] wayPoints)
-        {
-            _wayPointSystem = wayPointSystem;
-            _wayPoints = wayPoints;
-        }
-
-        public bool IsAtTheTargetPoint()
-        {
-            return _wayPointSystem.transform.position == _wayPoints[_wayPointSystem.WayPointIndex].transform.position;
-        }
-
-        public bool IsAtTheLastPoint()
-        {
-            return _wayPointSystem.transform.position == _wayPoints[_wayPoints.Length - 1].position;
-        }
-    }
 }
