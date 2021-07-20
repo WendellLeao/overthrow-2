148208a20b20bc21b2c33d91e421192554391509
Wendell Leão <leaowendell@hotmail.com.br>
2021-07-20 14:16:57 -0300

    Update CubesBackgroundRotation.cs

--- Assets/_Project/Scripts/Background/CubesBackgroundRotation.cs
+++ Assets/_Project/Scripts/Background/CubesBackgroundRotation.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public sealed class CubesBackgroundRotation : MonoBehaviour
 {
+    [SerializeField] private Transform _cubeTransform;
     [SerializeField] private float _xRotationSpeed, _yRotationSpeed, _zRotationSpeed;
 
     private void Update()
@@ -11,6 +12,14 @@ public sealed class CubesBackgroundRotation : MonoBehaviour
 
     private void HandleRotation()
     {
+<<<<<<< Updated upstream
         transform.Rotate(new Vector3(_xRotationSpeed, _yRotationSpeed, _zRotationSpeed) * Time.deltaTime);
+=======
+        ///(Done)
+        /// Fica a dica: se tu vai usar o transform no update como aqui é interessante tu dar um "cache" (guardar em uma variavel) o transform. Pode parecer estranho
+        /// mas believe it or not, é mais performatico do que acessar o transform como tu fez aqui embaixo.
+        /// Não é algo "game changer" por isso estou sugerindo só neste caso aqui em que estamos no update, em um metodo bem simples.
+        _cubeTransform.Rotate(new Vector3(_xRotationSpeed, _yRotationSpeed, _zRotationSpeed) * Time.deltaTime);
+>>>>>>> Stashed changes
     }
 }
